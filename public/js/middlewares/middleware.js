35d53fb0dfec8a76132c414b46d326be5fc04f45
TristoneAG <aram.guillen@tristone.com>
2026-05-08 15:19:25 -0600

    cookie fix

--- public/js/middlewares/middleware.js
+++ public/js/middlewares/middleware.js
@@ -32,7 +32,9 @@ middleware.verifyToken = (req, res, next) => {
                     res.cookie("accessToken", token_jwt, {
                         maxAge: 900000 /*10 Minutos*/,
                         httpOnly: false,
-                        secure: process.env.NODE_ENV === 'production' ? true : false
+                        secure: false,
+                        sameSite: 'lax',
+                        path: '/',
                     })
                     res.locals.authData = authData; next()
                 }
@@ -92,20 +94,50 @@ function validMac(mac) {
 
 middleware.macFromIP = (req, res, next) => {
     const regex = /::ffff:/gm;
-    let ip = (req.ip).replace(regex, "")
-    let localIp = (req.hostname).replace(regex, "")
-
-    if (ip === "::1" || ip === localIp) {
-        res.locals.macIP = { "mac": "00-00-00-00-00-00", "ip": "10.56.99.21" }; next()
+    // Get IP address with better fallback logic
+    let ip = null;
+    // Try different sources in order of preference
+    if (req.ip && req.ip !== '::1') {
+        ip = req.ip;
+    } else if (req.headers['x-forwarded-for']) {
+        ip = req.headers['x-forwarded-for'].split(',')[0].trim();
+    } else if (req.headers['x-real-ip']) {
+        ip = req.headers['x-real-ip'];
+    } else if (req.connection.remoteAddress && req.connection.remoteAddress !== '::1') {
+        ip = req.connection.remoteAddress;
+    } else if (req.socket.remoteAddress && req.socket.remoteAddress !== '::1') {
+        ip = req.socket.remoteAddress;
+    } else if (req.connection.socket && req.connection.socket.remoteAddress && req.connection.socket.remoteAddress !== '::1') {
+        ip = req.connection.socket.remoteAddress;
+    }
+    // Clean up the IP address
+    if (ip && typeof ip === 'string') {
+        ip = ip.replace(regex, "");
+        if (ip === '::1') {
+            ip = '127.0.0.1';
+        }
+    } else {
+        ip = '127.0.0.1';
+    }
+    let localIp = (req.hostname || 'localhost').replace(regex, "");
+    if (ip === "127.0.0.1" || ip === "::1" || ip === localIp) {
+        res.locals.macIP = { "mac": "00-00-00-00-00-00", "ip": "127.0.0.1" }; 
+        next();
     } else {
         macfromip.getMac(ip, (err, mac) => {
-
-            if (err == "The IP address cannot be self") {/* DO NOTHING*/ }
-
-            if (!validMac(mac)) {
-                res.render('mac_invalida.ejs', { mac })
+            if (err) {
+                if (err === "The IP address cannot be self") {
+                    res.locals.macIP = { "mac": "00-00-00-00-00-00", "ip": ip }; 
+                    next();
+                } else {
+                    res.locals.macIP = { "mac": "00-00-00-00-00-00", "ip": ip }; 
+                    next();
+                }
+            } else if (!validMac(mac)) {
+                res.render('mac_invalida.ejs', { mac });
             } else {
-                res.locals.macIP = { "mac": mac, "ip": ip }; next()
+                res.locals.macIP = { "mac": mac, "ip": ip }; 
+                next();
             }
         });
     }
